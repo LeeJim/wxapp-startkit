7792ef5bec15f1e63369df51133d8b44cc70763c
leejim <273743552@qq.com>
2017-06-06 17:14:35 +0800

    优化gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,60 @@
 var gulp = require('gulp')
 var babel = require('gulp-babel')
 var less = require('gulp-less')
-var rename = require("gulp-rename");
+var rename = require("gulp-rename")
+var del = require('del')
 
 gulp.task('css', () => {
-  return gulp.src('./src/pages/**/*.less')
+  return gulp.src('./src/**/*.less')
     .pipe(less())
     .pipe(rename({
       extname: ".wxss"
     }))
-    .pipe(gulp.dest('./dest/pages'))
+    .pipe(gulp.dest('./dest'))
 })
 
 gulp.task('js', () => {
-  return gulp.src('./src/pages/**/*.js')
+  return gulp.src('./src/**/*.js')
     .pipe(babel())
-    .pipe(gulp.dest('./dest/pages'))
+    .pipe(gulp.dest('./dest'))
 })
 
 gulp.task('html', () => {
-  return gulp.src('./src/pages/**/*.html')
+  return gulp.src('./src/**/*.html')
     .pipe(rename({
       extname: ".wxml"
     }))
-    .pipe(gulp.dest('./dest/pages'))
+    .pipe(gulp.dest('./dest'))
 })
 
 gulp.task('json', () => {
-  return gulp.src('./src/pages/**/*.json')
-    .pipe(gulp.dest('./dest/pages'))
+  return gulp.src('./src/**/*.json')
+    .pipe(gulp.dest('./dest'))
 })
 
-gulp.task('default', ['css', 'js', 'html', 'json'])
+gulp.task('clean', () => {
+  del('./dest')
+})
+
+// Task: default
+
+gulp.task('default', ['clean'], () => {
+  return gulp.start('css', 'js', 'html', 'json')
+})
 
 
-// watch
+// Task: watch
 
-gulp.watch('./src/pages/**/*.less', ['css'])
-gulp.watch('./src/pages/**/*.js', ['js'])
-gulp.watch('./src/pages/**/*.html', ['html'])
-gulp.watch('./src/pages/**/*.json', ['json'])
+gulp.task('dev', () => {
+
+  console.log('start development.')
+
+  gulp.watch('./src/pages/**/*.less', ['css'])
+  gulp.watch('./src/pages/**/*.js', ['js'])
+  gulp.watch('./src/pages/**/*.html', ['html'])
+  gulp.watch('./src/pages/**/*.json', ['json'])
+
+})
+
+
 
